e3141d2ea3072582e086c25456e2efe17cdc4e9b
pvictor <perrier.victor@gmail.com>
2019-03-07 10:31:57 +0100

    compute kivt

---
+++ R/compute_kivt.R
@@ -0,0 +1,61 @@
+
+#' Compute Kivt coefficient
+#'
+#' @param calendar Calendar data read with \code{\link{read_calendar}}.
+#' @param clusters_desc Clusters / groups description read with \code{\link{read_cluster_desc}}.
+#'
+#' @return a \code{data.table}
+#' @export
+#' 
+#' @importFrom data.table copy setorder := uniqueN 
+#'
+#' @examples
+#' \dontrun{
+#' # Calendar data
+#' calendar <- read_calendar(path = "REF_Planning_5_ans_mars_2018.xlsx")
+#' 
+#' # Clusters description
+#' clusters <- read_cluster_desc("HypothesesRTE_CHO-4145.xlsx")
+#' 
+#' 
+#' # Kivt computation
+#' kivt <- compute_kivt(calendar, clusters)
+#' kivt
+#' }
+compute_kivt <- function(calendar, clusters_desc) {
+  
+  calendar <- copy(calendar)
+  clusters_desc <- copy(clusters_desc)
+  
+  weekcal <- build_weekcal()
+  
+  weekcal[, .id := 1]
+  calendar[, .id := 1]
+  
+  week_groups <- weekcal[calendar[, list(
+    .id,
+    group = tranche,
+    shutdown_start = as.Date(date_debut),
+    shutdown_end = as.Date(date_de_fin_sans_prolongation)
+  )], on = ".id", allow.cartesian = TRUE]
+
+  week_groups[, n_overlaps := n_overlaps(week_start, week_end, shutdown_start, shutdown_end)]
+  
+  weekclus <- merge(
+    x = week_groups, 
+    y = clusters_desc[, list(group = corresp_groupes, type_groupe, pcn_mw)],
+    by = "group", all.x = TRUE, all.y = FALSE
+  )
+  weekclus <- weekclus[!is.na(type_groupe)]
+  weekclus[, pcn_mw := as.numeric(pcn_mw)]
+  
+  setorder(weekclus, week, group, -n_overlaps)
+  weekclus <- unique(weekclus, by = c("week", "group"))
+  
+  coef_kivt <- weekclus[, list(
+    kivt = sum(pcn_mw * n_overlaps / 7) / sum(pcn_mw) * 100,
+    n = .N, n_group = uniqueN(group)
+  ), by = list(week, week_start, week_end, type_groupe)]
+  setorder(coef_kivt, week_start, type_groupe)
+  coef_kivt[]
+}
--- R/zzz.R
+++ R/zzz.R
@@ -2,4 +2,6 @@
 utils::globalVariables(c("tranche", "date_de_fin_avec_prolongation", "date_de_fin_sans_prolongation",
                          "duree_prolongation_mean", "duree_prolongation_semaine", "duree_prolongation_jour",
                          ".SD", "palier", "n_days", "patterns", "nom", "pcn_mw", "type_groupe", "x7",
-                         "dates", "year", "week", "saturdays", "n", ".N"))
+                         "dates", "year", "week", "saturdays", "n", ".N",
+                         ".id", "corresp_groupes", "date_debut", "group", "shutdown_end", "shutdown_start",
+                         "week_end", "week_start"))
