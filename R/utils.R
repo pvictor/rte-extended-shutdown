ffe7d6c1014245c91658401beb0b62fad2efc72e
pvictor <perrier.victor@gmail.com>
2019-03-21 14:26:05 +0100

    update n_overlaps

--- R/utils.R
+++ R/utils.R
@@ -6,8 +6,9 @@ n_days_overlap <- function(x, y) {
 }
 
 n_overlaps <- function(w1, w2, s1, s2) {
-  x1 <- (as.numeric(w2 - s1) + 1) * (s1 > w1 & s1 < w2)
-  x2 <- (as.numeric(s2 - w1) + 1) * (s2 > w1 & s2 < w2)
+  x1 <- (as.numeric(w2 - s1) + 1) * (s1 > w1 & s1 < w2 & s2 >= w2)
+  x2 <- (as.numeric(s2 - w1) + 1) * (s2 > w1 & s2 < w2 & s1 <= w1)
   x3 <- 7 * (s1 <= w1 & s2 >= w2)
-  x1 + x2 + x3
+  x4 <- (as.numeric(s2 - s1) + 1) * (s1 > w1 & s2 < w2)
+  x1 + x2 + x3 + x4
 }
